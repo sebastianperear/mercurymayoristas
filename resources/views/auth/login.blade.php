dd9b02e036bab18964bf61ca60ad2ed2c3a58811
Sebastian Perea <sebasperea1@gmail.com>
2020-09-09 15:38:52 -0500

    VO.0.1.1

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,7 +25,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Iniciar sesión') }}</div>
+                <div class="card-header">{{ __('Iniciar sesión.') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
